cfc823af988cedbf95e21907c2854fd39197a628
Dousiii <84726282+Dousiii@users.noreply.github.com>
2024-11-01 23:36:17 -0700

    Add second feature

--- temp.py
+++ temp.py
@@ -1,8 +1,14 @@
 while True:
     choice = input("Please enter: \n(1) for C to F \n(2) for F to C \n (3) to quit: ")
     if choice == '1':
+        c = float(input("Enter the temperature in Celsius: "))
+        f = (c * 9/5) + 32
+        print(f"{c}°C is {f}°F")
         break
     elif choice == '2':
+        f = float(input("Enter the temperature in Fahrenheit: "))
+        c = (f - 32) * 5/9
+        print(f"{f}°F is {c}°C")
         break
     elif choice == '3':
         break
