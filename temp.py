6e282f70344556adbd8968c0f2322bf157ab509d
Dousiii <84726282+Dousiii@users.noreply.github.com>
2024-11-01 23:23:44 -0700

    Initial Commit

---
+++ temp.py
@@ -0,0 +1,10 @@
+while True:
+    choice = input("Please enter: \n(1) for C to F \n(2) for F to C \n (3) to quit: ")
+    if choice == '1':
+        break
+    elif choice == '2':
+        break
+    elif choice == '3':
+        break
+    else:
+        print("Invalid input. Please try again.")
